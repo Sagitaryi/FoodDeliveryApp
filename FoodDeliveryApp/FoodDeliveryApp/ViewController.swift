2d33f4b06d696ffecf91056539c5d0679039f7c4
Chumakov Aleksey <Alexeyse10@gmail.com>
2024-02-25 23:45:14 +0700

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Алексей Чумаков on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
